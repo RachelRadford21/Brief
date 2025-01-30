bc231527e8e35aaff7fe9a3e47eb89306af7254a
Rachel Radford <rachelaradford21@gmail.com>
2025-01-30 18:19:44 -0500

    small refactor

---
+++ Brief/View/ArticleTitleView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleTitleView.swift
+//  Brief
+//
+//  Created by Rachel Radford on 1/30/25.
+//
+
+import SwiftUI
+
+struct ArticleTitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticleTitleView()
+}
