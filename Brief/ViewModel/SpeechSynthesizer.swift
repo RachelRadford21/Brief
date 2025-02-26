3aa7ee0b774afb228a4b01926f2096aa24822556
Rachel Radford <rachelaradford21@gmail.com>
2025-02-25 20:46:25 -0500

    commit

--- Brief/ViewModel/SpeechSynthesizer.swift
+++ Brief/ViewModel/SpeechSynthesizer.swift
@@ -15,13 +15,13 @@ class SpeechSynthesizer: ObservableObject {
     func speak(_ text: String) {
         let utterance = AVSpeechUtterance(string: text)
         // Add to settings so user has ability to change speed and voice and pitch multiplier
-        // Add siri
+       
+        utterance.voice = AVSpeechSynthesisVoice(identifier: "en-US")
 //        utterance.voice = AVSpeechSynthesisVoice(identifier: "com.apple.voice.compact.en-AU.Gordon")
       //  utterance.voice = AVSpeechSynthesisVoice(identifier: "com.apple.voice.compact.en-IE.Moira")
         utterance.rate = speechSpeed
         utterance.pitchMultiplier = 1.15
         synthesizer.speak(utterance)
-        stopSpeaking()
     }
     
     func stopSpeaking() {
